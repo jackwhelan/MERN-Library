1a14f40d6fc7720912dd16cb067b4bbeeb1c66da
jackwhelan <jackw97@live.ie>
2020-04-24 18:49:40 +0100

    Added info/alert component

---
+++ frontend/src/components/Alert.component.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            style: {}
+        }
+    }
+
+    componentDidMount() {
+        var bgColor;
+
+        if (this.props.response.status.includes("error")) {
+            bgColor = '#d9534f';
+        }
+        else if (this.props.response.status.includes("success")) {
+            bgColor = '#5cb85c';
+        }
+        else {
+            bgColor = '#d9534f';
+        }
+
+        this.setState({
+            style: {
+                backgroundColor: bgColor
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="infoComponent" style={this.state.style}>
+                <h5>{this.props.response.header}</h5>
+                <p>{this.props.response.message}</p>
+            </div>
+        )
+    }
+}
+
+export default Alert;
