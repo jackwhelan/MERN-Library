346f85fcc47cc4bdbf9996e648d9dcd3c93378eb
jackwhelan <jackw97@live.ie>
2020-04-24 18:50:18 +0100

    Added login view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomeView from './views/Home.view';
 import RegisterView from './views/Register.view';
+import LoginView from './views/Login.view';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
             <Route path="/" exact component={HomeView} />
             <Route path="/Register" component={RegisterView} />
+            <Route path="/Login" component={LoginView} />
         </Switch>
     </Router>
   );
---
+++ frontend/src/views/Login.view.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TopNav from '../components/TopNav.component';
+
+class LoginView extends Component {
+    render() {
+        return (
+            <div>
+                <TopNav/>
+                login
+            </div>
+        )
+    }
+}
+
+export default LoginView;
