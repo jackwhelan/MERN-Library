23a06e972df1dbd9bb54654f317d8ebacb722ed9
jackwhelan <jackw97@live.ie>
2020-04-24 15:55:37 +0100

    Restructured server

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,22 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const express = require('express');
 
+// Configuration
+dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-dotenv.config();
-
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+// API Routes
 const UserRoute = require('./routes/User.route');
+app.use('/user', UserRoute);
 
+// Database Connection
 mongoose
     .connect(process.env.MONGO_URI, {
         useUnifiedTopology: true,
@@ -29,8 +32,7 @@ mongoose
         console.log(`DB Connection Error: ${err.message}`);
     });
 
-app.use('/user', UserRoute);
-
+// Server Listening
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
 });
