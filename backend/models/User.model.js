5bbb1d6ed5559805308833da446cbd3389cb7314
jackwhelan <jackw97@live.ie>
2020-04-24 15:20:46 +0100

    Added user model

---
+++ backend/models/User.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+exports.User = User;
