3fbd5acd390befc0709167e2e69c78f9d6084c98
jackwhelan <jackw97@live.ie>
2020-04-24 15:25:42 +0100

    Added routes to router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import HomeView from './views/Home.view';
+import RegisterView from './views/Register.view';
 
 function App() {
   return (
+    <Router>
+        <Switch>
+            <Route path="/" exact component={HomeView} />
+            <Route path="/Register" component={RegisterView} />
+        </Switch>
+    </Router>
   );
 }
 
