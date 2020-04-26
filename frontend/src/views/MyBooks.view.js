aaa6ccbf1d1c0b4823e1a85f7e922269cc48e2f1
jackwhelan <jackw97@live.ie>
2020-04-26 19:02:50 +0100

    added mybooks view

---
+++ frontend/src/views/MyBooks.view.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import AppNavbar from '../components/AppNavbar.component';
+import Alert from '../components/Alert.component';
+import { Redirect } from 'react-router-dom';
+
+class MyBooksView extends Component {
+    state = {
+        info: undefined
+    }
+
+    componentDidMount() {
+        if (this.props.location.state) {
+            this.setState({
+                info: this.props.location.state
+            })
+        }
+
+        if (!localStorage.getItem('TOKEN')) {
+            this.setState({
+                redirect: true
+            });
+        }
+    }
+
+    render() {
+        let alertIfInfo;
+
+        if (this.state.info) {
+            alertIfInfo = <Alert response={this.state.info} />
+        }
+
+        if (this.state.redirect === true) {
+            return <Redirect to={{
+                pathname: "/",
+                state: {
+                    status: "warning",
+                    header: "Access Restricted",
+                    message: "You must be signed in to access the Library page."
+                }
+            }} />
+        }
+
+        return (
+            <div>
+                <AppNavbar token={localStorage.getItem('TOKEN')} />
+                <div className="container">
+                    {alertIfInfo}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MyBooksView;
